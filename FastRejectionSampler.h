ebb28042b30ee663df9d9905bd147be9b5d7c375
elyawy <elya.wygoda@gmail.com>
2024-06-02 15:36:22 +0300

    more changes

--- FastRejectionSampler.h
+++ FastRejectionSampler.h
@@ -7,7 +7,7 @@
 
 #include <iostream>
 
-#include "avl_array.h"
+// #include "avl_array.h"
 
 class FastRejectionSampler
 {
@@ -19,22 +19,27 @@ private:
     std::uniform_real_distribution<double> distribution;
 
     double _totalWeightsSum;
+    int _minWeightLevel;
+    int _maxWeightLevel;
+
     std::vector<double> _levelsCDF;
 
 public:
     FastRejectionSampler(const std::vector<double> &weights): _weights(weights) {
 
         auto it = std::minmax_element(std::begin(_weights), std::end(_weights)); // C++11
-        int minWeightLevel = static_cast<int>(std::log2(*it.first)) + 1;
-        int maxWeightLevel = static_cast<int>(std::log2(*it.second)) + 1;
-        size_t numLevels = std::log2((*it.second) / (*it.first));
+        _minWeightLevel = static_cast<int>(std::log2(*it.first)) + 1;
+        _maxWeightLevel = static_cast<int>(std::log2(*it.second)) + 1;
+        size_t numLevels = _maxWeightLevel - _minWeightLevel + 1;
 
+        std::cout << "min level=" << _minWeightLevel << "\n";
+        std::cout << "max level=" << _maxWeightLevel << "\n";
         std::cout << "num levels=" << numLevels << "\n";
 
         _levelToWeights.reserve(numLevels);
         _levelsCDF.resize(numLevels, 0.0);
 
-        for (int i = minWeightLevel; i <= maxWeightLevel; ++i) {
+        for (int i = _minWeightLevel; i <= _maxWeightLevel; ++i) {
             _levelToWeights[i] = std::make_pair<double, std::vector<double>>(0.0, {});
         }
 
@@ -44,18 +49,15 @@ public:
             int level = static_cast<int>(std::log2(weight)) + 1;
             _levelToWeights.at(level).first += weight;
             _levelToWeights.at(level).second.push_back(weight);
-
-            std::cout << level << " " << _levelToWeights.at(level).first << "\n";
         }
 
         double cumulativeSumOfWeights = 0.0;
-        std::cout << cumulativeSumOfWeights << "\n";
-        for (int level = minWeightLevel; level <= maxWeightLevel; ++level) {
-             cumulativeSumOfWeights += _levelToWeights.at(level).first;
-             std::cout << level - minWeightLevel << "->" << cumulativeSumOfWeights << "\n";
-
-             _levelsCDF[level - minWeightLevel] = (cumulativeSumOfWeights);
+        for (int level = _minWeightLevel; level <= _maxWeightLevel; ++level) {
+            if (_levelToWeights.at(level).first == 0.0) continue;
+            cumulativeSumOfWeights += _levelToWeights.at(level).first;
+            _levelsCDF[level - _minWeightLevel] = (cumulativeSumOfWeights);
         }
+        // std::sort(_levelsCDF.begin(), _levelsCDF.end());
     }
     
 
@@ -67,6 +69,10 @@ public:
 
     double getLevelWeight(int level) {
         return _levelToWeights.at(level).first;
+    }
+
+    int getLevelBin(int level) {
+        return level + _minWeightLevel;
     }
 
 
