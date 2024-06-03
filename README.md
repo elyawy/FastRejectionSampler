c7dc77739025043096a67a7415c80bf48bd903a2
elyawy <elya.wygoda@gmail.com>
2024-06-03 15:28:48 +0300

    version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FastRejectionSampler
+
+A C++ implementation of the ideas presented [here](https://www.aarondefazio.com/tangentially/?p=58#comment-240).
+I welcome any improvements!
--- test.cpp
+++ test.cpp
@@ -1,19 +1,52 @@
 
+
 #include "FastRejectionSampler.h"
 // #include "avl_array.h"
 #include <iostream>
+#include <random>
+#include <map>
 
+#include <iomanip>
 // #include <unordered_set>
 // #include <cmath>
 int main() {
 
     FastRejectionSampler fars({6.8, 0.03,20.1,0.5,1.7,4.2,10.3});
 
-    auto levels = fars.getLevelsCDF();
-    for (size_t i = 0; i < levels.size(); i++)
-    {
-        std::cout << i << " " <<  levels.at(i) << "\n";//<< "->" << fars.getLevelWeight(i-4) << "\n";
-    }
+    auto gen = std::mt19937_64(0);
+
+
+    size_t numSamples = 10000;
+    std::map<int, int> hist;
+    for (int n = 0; n != numSamples; ++n)
+        ++hist[fars.sample(gen)];
+ 
+    for (auto const& [x, y] : hist)
+            std::cout  << x << " -> "  <<  y   << '\n';
+    
+    std::cout << "\n";
+    fars.updateWeight(1, 15.0);
+
+    hist.clear();
+
+    for (int n = 0; n != numSamples; ++n)
+        ++hist[fars.sample(gen)];
+ 
+    for (auto const& [x, y] : hist)
+            std::cout  << x << " -> "  <<  y   << '\n';
+
+    std::cout << "\n";
+    fars.updateWeight(5, 42.0);
+
+    hist.clear();
+
+    for (int n = 0; n != numSamples; ++n)
+        ++hist[fars.sample(gen)];
+ 
+    for (auto const& [x, y] : hist)
+            std::cout  << x << " -> "  <<  y   << '\n';
+
+
 
 
     return 0;
